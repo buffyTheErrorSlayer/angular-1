7b166f755d34e8b2b5dcec7a6815087918e569ff
Mariana <mariana.barbosa@90tech.fr>
2024-08-27 10:56:32 +0200

    add recapitulatif

--- src/app/recap/recap.component.ts
+++ src/app/recap/recap.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './recap.component.scss'
 })
 export class RecapComponent {
+  tasks: { id: number; title: string; completed: boolean }[] = [];
+  ongoingTasksCount = 0;
+  doneTasksCount = 0;
+
+  ngOnInit(){
+    const storedTasks = localStorage.getItem('tasks');
+    if(storedTasks){
+      this.tasks = JSON.parse(storedTasks);
+    }
+    
+      this.doneTasksCount = this.filterDoneTasks().length;
+      this.ongoingTasksCount = this.tasks.length - this.doneTasksCount;
+  }
+
+  filterDoneTasks(){
+    return this.tasks.filter(task => task.completed === true);
+  }
+
+  filterOngoingTasks(){
+    return this.tasks.filter(task => task.completed === false);
+  }
 
 }
