d804d54b2165d192672afb7185eca7e09ac9d6c9
Mariana <mariana.barbosa@90tech.fr>
2024-08-27 09:30:27 +0200

    add new componentes routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { Page404Component } from './page404/page404.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { RecapComponent } from './recap/recap.component';
 
 
 export const routes: Routes = [
     {path: "accueil", component: AccueilComponent},
     {path: "connexion", component: ConnexionComponent},
     {path: "", redirectTo: "accueil", pathMatch: 'full' },
+    {path: "to-do", component: TodolistComponent},
+    {path: "recap", component:RecapComponent},
     {path: "**", component: Page404Component}
 ];
