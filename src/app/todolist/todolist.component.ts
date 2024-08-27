237e6b4adeaa207c08c52f01f6fd911c606d903d
Mariana <mariana.barbosa@90tech.fr>
2024-08-27 09:56:20 +0200

    add adding task to local storage

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.scss'
 })
 export class TodolistComponent {
+  tasks: { id: number; title: string; completed: boolean }[] = [];
+  newTask : string = '';
+
+  addTask(title: string): void {
+    this.tasks.push({ id: Date.now(), title, completed: false });
+    this.sauvegarde()
+  }
+
+  removeTask(id: number): void {
+    this.tasks = this.tasks.filter(task => task.id!== id);
+    this.sauvegarde()
+  }
+
+  sauvegarde() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 
 }
